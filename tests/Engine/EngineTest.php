2c7fa258f60a0e78e0e850ccadefbf456e87f6bc
Thomas Müller <mimmi20@live.de>
2020-05-13 08:45:01 +0200

    update dependencies and pipelines

--- tests/Engine/EngineTest.php
+++ tests/Engine/EngineTest.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the ua-result package.
  *
- * Copyright (c) 2015-2019, Thomas Mueller <mimmi20@live.de>
+ * Copyright (c) 2015-2020, Thomas Mueller <mimmi20@live.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -11,7 +11,6 @@
 declare(strict_types = 1);
 namespace UaResultTest\Engine;
 
-use BrowserDetector\Version\Version;
 use BrowserDetector\Version\VersionInterface;
 use PHPUnit\Framework\TestCase;
 use UaResult\Company\CompanyInterface;
--- tests/Result/ResultTest.php
+++ tests/Result/ResultTest.php
@@ -2,7 +2,7 @@
 /**
  * This file is part of the ua-result package.
  *
- * Copyright (c) 2015-2019, Thomas Mueller <mimmi20@live.de>
+ * Copyright (c) 2015-2020, Thomas Mueller <mimmi20@live.de>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -12,13 +12,9 @@ declare(strict_types = 1);
 namespace UaResultTest\Result;
 
 use PHPUnit\Framework\TestCase;
-use UaResult\Browser\Browser;
 use UaResult\Browser\BrowserInterface;
-use UaResult\Device\Device;
 use UaResult\Device\DeviceInterface;
-use UaResult\Engine\Engine;
 use UaResult\Engine\EngineInterface;
-use UaResult\Os\Os;
 use UaResult\Os\OsInterface;
 use UaResult\Result\Result;
 
